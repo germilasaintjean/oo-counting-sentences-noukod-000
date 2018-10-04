89528f59050878ead6ff0ba41d80a2bc9657bc61
Saint-Jean Germila <germila@noukod.com>
2018-10-04 13:57:13 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,22 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-   if "HI!".end_with?("!")
-    true
-  else
-  false
+    self.end_with?(".")
   end
-end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
